05a9e7d6ad196192a2817547a49bca4ebd448532
Pkt-Rkt <139321742+Pkt-Rkt@users.noreply.github.com>
2023-10-17 20:13:33 -0700

    Calc program

---
+++ lab3.js
@@ -0,0 +1,23 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    const input = display.value;
+    try {
+        const result = eval(input);
+        if (result === undefined || isNaN(result)) {
+            display.value = 'Error';
+        } else {
+            display.value = result;
+        }
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
